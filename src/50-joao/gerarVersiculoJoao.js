3c6bef7186997bc1a68897bd4e2e189dd55a0171
Ranniêr Reis <rannier@mobilesaude.com.br>
2023-05-25 11:03:43 -0300

    versiculosDeBolso - implement 50-joao

---
+++ src/50-joao/gerarVersiculoJoao.js
@@ -0,0 +1,39 @@
+function gerarNumerosAleatorios(quantidade, min, max) {
+    var numeros = [];
+
+    for (var i = 0; i < quantidade; i++) {
+      var numero = Math.floor(Math.random() * (max - min + 1)) + min;
+      numeros.push(numero);
+    }
+
+    return numeros;
+  }
+
+function fetchVersiculo(){
+var capitulo = gerarNumerosAleatorios(1, 1, 21);
+var versiculo = gerarNumerosAleatorios(1, 1, 25);
+
+
+fetch(`https://bible-api.com/joao${capitulo}:${versiculo}?translation=almeida`)
+    .then(response => {
+      if(response.ok){
+        return response.json()
+      } else if(response.status === 404){
+        return fetchVersiculo()
+      } else {
+        throw new Error('Requisiçào falhou com status:' + response.status)
+      }
+    })
+    .then( data =>{
+      var versiculoElement = document.getElementById('versiculo');
+      versiculoElement.textContent= data.text;
+      var versiculoReference = document.getElementById('reference')
+      versiculoReference.textContent = data.reference;
+    })
+    .catch( error => {
+      console.log('Ocorreu um erro: '+ error.message)
+    })
+
+}
+
+fetchVersiculo()
